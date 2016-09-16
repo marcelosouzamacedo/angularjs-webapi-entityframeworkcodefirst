f2d7e5b01931d2cc54575162882f251d251f37e2
Jonathas Costa <jonathas.costa@gmail.com>
2016-09-16 00:58:58 -0300

    Configuração inicial de de rotas e actions básicas

---
+++ Api/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        private ShoppingCartContext db = new ShoppingCartContext();
+
+        // GET: api/Categories
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories;
+        }
+
+        // GET api/Category/5/Categories
+        [Route("api/Category/{id}/Categories")]
+        public IQueryable<Category> GetChildren(int id)
+        {
+            return db.Categories.Where(c => c.Parent.Id == id);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Api/Models/Category.cs
+++ Api/Models/Category.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace Api.Models
 {
     public class Category
     {
-        public Category()
-        {
-            Products = new List<Product>();
-        }
-
         public int Id { get; set; }
         public string Name { get; set; }
 
         public Category Parent { get; set; }
         public ICollection<Product> Products { get; set; }
-        public ICollection<Category> ChildrenCategories { get; set; }
     }
 }
